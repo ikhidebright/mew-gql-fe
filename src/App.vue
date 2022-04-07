5b19064146cfdff8cebd30bf182fb6db553c66ee
bright.ikhide <bright.ikhide@e4email.net>
2022-04-07 21:25:32 +0100

    added card to display weather info, button to refresh weather details, and made calls to the server

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <v-card
+        class="mx-auto mt-15"
+        max-width="344"
+        variant="outlined"
+        rounded="0"
+      >
+        <v-card-text>
+          <p v-if="loading">loading...</p>
+          <p v-else-if="error">Sorry an error occurred</p>
+          <p v-else>Right now it is {{ temp }} degrees in LA</p>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            variant="outlined"
+            @click="refetch()"
+            :disabled="loading"
+            rounded="0"
+          >
+            Update
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-main>
+  </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import { useQuery, useResult } from "@vue/apollo-composable";
+import gql from "graphql-tag";
+import { ref } from "vue";
+import { GET_WEATHER_QUERY } from "./graphql/query/weather.query";
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+const cityName = ref("Los Angeles");
+const weather = gql(GET_WEATHER_QUERY);
+const { loading, result, error, refetch } = useQuery(
+  weather,
+  {
+    cityName: cityName,
+  },
+  {
+    fetchPolicy: "cache-and-network",
   }
-}
+);
+const temp = useResult(result, null, (data) => {
+  return Math.floor(data.getCurrentWeatherByCityName - 273.15);
+});
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
