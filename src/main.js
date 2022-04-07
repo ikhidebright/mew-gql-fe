b30a4a1ad2fc465a0b701eb302194ebd38b05155
bright.ikhide <bright.ikhide@e4email.net>
2022-04-07 21:26:21 +0100

    added vuetify and also apolloclient

--- src/main.js
+++ src/main.js
@@ -1,4 +1,19 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp, provide, h } from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
+import { ApolloClients } from "@vue/apollo-composable";
+import { apolloClient } from "../src/graphql/apollo.provider";
 
-createApp(App).mount('#app')
+loadFonts();
+
+const app = createApp({
+  setup() {
+    provide(ApolloClients, {
+      default: apolloClient,
+    });
+  },
+  render: () => h(App),
+});
+app.use(vuetify);
+app.mount("#app");
